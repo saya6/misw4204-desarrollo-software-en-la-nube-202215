bcaf3b68d122cd89543b339dab0664868af3a35c
cgarciaa1 <ca.garciaa1@uniandes.edu.co>
2022-10-17 20:19:26 -0500

    Corrección de api

--- conversion-service/app.py
+++ conversion-service/app.py
@@ -10,7 +10,6 @@ celery = ext_celery.celery
 
 api.add_resource(AuthenticationResource, '/users/authenticate')
 api.add_resource(SignInResource, '/users/signin')
-api.add_resource(ConversionTaskResource, '/api/task')
 api.add_resource(ConversionTaskResource, '/api/tasks','/api/tasks/<int:order>', '/api/tasks/<int:order>/<int:max>')
 api.add_resource(FileRetrieverResource, '/api/files/<string:filename>')
 
