99e3f5cb5d9c1f0dd9058ff3c92c84894195fdb1
saya6 <s.ayat@uniandes.edu.co>
2022-11-27 17:29:22 +0000

    [FEATURE] Ajustes para correr en app engine

--- conversion-service/run_daemon.py
+++ conversion-service/run_daemon.py
@@ -1,10 +1,18 @@
 from multiprocessing import Process
-from app import app
-from project import ext_celery
+from app import app as current_flask_app
 from project.task_daemon.daemon import run_daemon
 
-celery = ext_celery.celery
+from flask import Flask
 
-p1 = Process(target=run_daemon, args=(app,))
+app = Flask(__name__)
+
+p1 = Process(target=run_daemon, args=(current_flask_app,))
 p1.start()
-p1.join()
+
+@app.route('/')
+def main():
+    return {"service": "worker", "status": "ok"}, 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    p1.close()
